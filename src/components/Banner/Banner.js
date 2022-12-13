f518a50915110324c134ecc25de0f8504eca23eb
Rejiane <rejifredrez@gmail.com>
2022-12-12 23:59:46 -0300

    added banner

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,14 @@
+import "./Banner.css";
+
+function Banner() {
+  return (
+    <header className="banner">
+      <img
+        src="/images/banner.png"
+        alt="O banner principal de página do Organo."
+      ></img>
+    </header>
+  );
+}
+
+export default Banner;
